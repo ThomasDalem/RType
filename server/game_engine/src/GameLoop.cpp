21564813b11ef1a77c8599fa545075492373ad5f
Alexandre Pereira <apereira@localhost.localdomain>
2020-11-10 11:45:40 +0100

    [UPD] create power upon ennemy death and add sprite rect for player

--- server/game_engine/src/GameLoop.cpp
+++ server/game_engine/src/GameLoop.cpp
@@ -52,24 +52,24 @@ void game_engine::GameLoop::sendToClients()
     std::vector<std::shared_ptr<AComponents>> entitiesComponents;
     game_engine::Player *player;
     Transform *entitieTransfromComponent;
-    Collision *entitieCollisionComponent;
+    Render *entitieRenderComponent;
     Health *entitieHealthComponent;
 
     network::UDPClientMessage clientMessage;
 
     for (entitiesListIter = _entities->begin(); entitiesListIter != _entities->end(); entitiesListIter++) {
         entitiesComponents = entitiesListIter->get()->getComponentList();
-        getComponentToDisp(entitiesComponents, entitieTransfromComponent, entitieCollisionComponent);
+        getComponentToDisp(entitiesComponents, entitieTransfromComponent, entitieRenderComponent);
         clientMessage.entitieType = entitiesListIter->get()->getEntitiesID();
         clientMessage.uniqueID = entitiesListIter->get()->getUniqueID();
         clientMessage.value[0] = 1;
         clientMessage.value[1] = entitieTransfromComponent->getPosition().x;
         clientMessage.value[2] = entitieTransfromComponent->getPosition().y;
         clientMessage.value[3] = entitieTransfromComponent->getRotation();
-        clientMessage.value[4] = entitieCollisionComponent->getRectSize().x;
-        clientMessage.value[5] = entitieCollisionComponent->getRectSize().y;
-        clientMessage.value[6] = entitieCollisionComponent->getRectSize().L;
-        clientMessage.value[7] = entitieCollisionComponent->getRectSize().l;
+        clientMessage.value[4] = entitieRenderComponent->getRect().x;
+        clientMessage.value[5] = entitieRenderComponent->getRect().y;
+        clientMessage.value[6] = entitieRenderComponent->getRect().L;
+        clientMessage.value[7] = entitieRenderComponent->getRect().l;
         server.broadcastDispClassMessage(clientMessage);
     }
     for (playerListIter = playersList->begin(); playerListIter != playersList->end(); playerListIter++) {
@@ -88,14 +88,15 @@ void game_engine::GameLoop::sendToClients()
     }
 }
 
-void game_engine::GameLoop::getComponentToDisp(std::vector<std::shared_ptr<AComponents>> componentList, Transform *transfromComponent, Collision *collisionComponent)
+void game_engine::GameLoop::getComponentToDisp(std::vector<std::shared_ptr<AComponents>> componentList, Transform *transfromComponent, Render *renderComponent)
 {
     std::vector<std::shared_ptr<AComponents>>::iterator componentListIter;
+
     for (componentListIter = componentList.begin(); componentListIter != componentList.end(); ++componentListIter) {
         if (componentListIter->get()->getType() == ComponentType::TRANSFORM)
             transfromComponent = static_cast<Transform *>(componentListIter->get());
-        if (componentListIter->get()->getType() == ComponentType::COLLISION)
-            collisionComponent = static_cast<Collision *>(componentListIter->get());
+        if (componentListIter->get()->getType() == ComponentType::RENDER)
+            renderComponent = static_cast<Render *>(componentListIter->get());
     }
 }
 
