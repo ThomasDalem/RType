1911741602be8a58830e32155118015579797ca7
Roméo <romeo@localhost.localdomain>
2020-11-11 16:20:28 +0100

    [MRG]

---
+++ client/Class/Animation.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2020
+** B-CPP-501-PAR-5-1-rtype-romeo.talovici
+** File description:
+** Animation
+*/
+
+#ifndef ANIMATION_HPP_
+#define ANIMATION_HPP_
+
+#include "Client.hpp"
+
+class client::Animation {
+    public:
+        Animation();
+        ~Animation();
+
+    private:
+    protected:
+};
+
+#endif
